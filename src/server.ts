063e20776348af5aff8990982b1da479344862ec
GiovaniGama <giovanigama.08@hotmail.com>
2023-03-19 01:55:09 -0300

    task/1 -- ajustando docker

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0'
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.send('steste teste teste')
+    return res.send('stests sdadasteste teste')
 })
 
 app.listen(PORT, HOST)
